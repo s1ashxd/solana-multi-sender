188b0f97a1e92f854a00b73a65e224e9fe5aaece
s1ashxd <s1ash.xd224@gmail.com>
2026-05-24 10:28:19 +0300

    scaffold tx-sender crate

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod backend;
+pub mod conn;
+pub mod error;
+pub mod job;
+pub mod protocol;
+pub mod provider;
+pub mod result_lane;
+pub mod sink;
+pub mod source;
+pub mod transport;
+
+pub mod prelude {}
